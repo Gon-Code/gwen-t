48252c832bfef2e043b89478ef97a03336f497c1
gonzalo <gonzalo.javier.cartes@gmail.com>
2023-04-24 23:00:42 -0400

    Redefinicion de la clase player Test, metodos completamente implementados . Se logra inicializar correctamente la clase PlayerClass

--- src/main/scala/gwent/PlayerClass.scala
+++ src/main/scala/gwent/PlayerClass.scala
@@ -14,8 +14,8 @@ import scala.util.control.Breaks.break
 //La idea de usar ListBuffer es que al momento de mover cartas del deck o la mano, debemos eliminarlas
 //Esto es mucho mas sencillo con ListBuffer que con List.
 
-abstract class AbstractPlayer(val name:String, val side:String, val gem :Int, val deck : ListBuffer[AbstractCard], val hand : ListBuffer[AbstractCard])
-extends Player{
+class PlayerClass(val name:String, val side:String, val gem :Int, val deck : ListBuffer[AbstractCard], val hand : ListBuffer[AbstractCard])
+extends Player {
   //El metodo play, saca una carta de la mano y la coloca en el tablero
   def play(s : String) : Unit={
     //String representa el nombre de la carta que debemos sacar, pero primero debemos ubicarla en el mazo para eliminarla
@@ -35,7 +35,7 @@ extends Player{
     //Generamos un numero entre 0 y el de la cantidad de elementos en el deck
     val x = Random.between(0, deck.length + 1)
     //card_played es la carta que se mueve desde el deck hacia la mano
-    val card_drew = List(x)
+    val card_drew = deck(x)
     //Agregamos la carta robada a la mano
     hand += card_drew
     //Eliminamos la carta robada del deck
--- src/test/scala/PlayerTest.scala
+++ src/test/scala/PlayerTest.scala
@@ -1,4 +1,11 @@
 package cl.uchile.dcc
 package gwent
 
+import scala.collection.mutable.ListBuffer
 
+class PlayerTest extends munit.FunSuite {
+  test("La clase jugador inicializa correctamente"){
+    var jugador = new PlayerClass("Javier","A",3,ListBuffer[AbstractCard](),ListBuffer[AbstractCard]())
+    assertEquals("Javier",jugador.name)
+  }
+}
