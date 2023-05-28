11af976a6f95c366e2dd6bb43e2201b657fc2ccb
gonzalo <gonzalo.javier.cartes@gmail.com>
2023-05-28 05:11:10 -0400

    Testeo de la clase Player
    Se han añadido los test correspondientes para la clase Player. Los test implementados generan un 100% de coverage al probar todos los métodos y parámetros de la clase Player , así como también funcionalidades intrínsecas del juego como por ejemplo :
    -El parámetro gem no puede ser negativo (Para pasar este test se agregó código a la clase Player)
    -No puede haber más de 10 cartas en la mano(Es decir, el tamaño del parámetro hand no puede ser mayor a 10)
    En la siguiente actualización se crearan los test para todos los tipos de cartas ! 😊

--- src/main/scala/gwent/Player.scala
+++ src/main/scala/gwent/Player.scala
@@ -10,32 +10,34 @@ import gwent.cards.Card
  *
  * @param name The name of the player.
  * @param gem The amount of gems the player has . When this parameter is equal to 0, is because this player has lost the game.
- * @param _deck Represents the cards that are in the deck.
- * @param _hand Represents the cards the player has in hand.
+ * @param deck Represents the cards that are in the deck.
+ * @param hand Represents the cards the player has in hand.
  *
  * @constructor Creates a new player with the specified name,gem,_deck and _hand.
  *
- *
- *
  */
-class Player (val name:String , val gem :Int, private var _deck : List[Card],private var _hand : List[Card]) {
-
+class Player (val name:String , var gem :Int, private var deck : List[Card],private var hand : List[Card]) {
   /** Accessor method for the player's deck */
-  private def deck: List[Card] = _deck
+  def getDeck: List[Card] = deck
 
   /** Accessor method for the player's hand */
-  private def hand: List[Card] = _hand
+  def getHand: List[Card] = hand
+  
+  /** The gem count can't be lower than zero */
+  if(gem < 0){
+    gem = 0
+  }
 
   /** Take the first card in the _deck and adds it to the _hand. */
   def drawCard(): Card = {
     val card = deck.head
-    _deck = deck.tail
-    _hand = card :: hand
+    deck = deck.tail
+    hand = card :: hand
     card
   }
 /** Shuffles the deck. */
   def shuffleDeck(): Unit = {
-    _deck = scala.util.Random.shuffle(_deck)
+    deck = scala.util.Random.shuffle(deck)
   }
 
 }
